3bd28d9f5df454cb18621719a637dcf193c396e8
Jose Soto <jsoto7087@gmail.com>
2021-02-20 18:19:16 -0500

    commit tree a4229986c86c5ae27f384c0e1575b8d89258bf0e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,54 @@
 
+class Book
+    attr_accessor :author, :page_count, :genre, :turn_page
+    attr_reader :title
 
+
+  def initialize(title)
+    @title = title 
+
+  end 
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Book
+#     attr_accessor :author, :page_count, :genre
+  
+#     def initialize(title)
+#       @title = title
+#     end
+  
+#     def title
+#       @title
+#     end
+  
+#     def turn_page
+#       puts "Flipping the page...wow, you read fast!"
+#     end
+  
+#   end 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,77 @@
 # Make your shoe class here!
+
+class Shoe
+
+  attr_accessor :brand, :color, :size, :material
+  # attr_reader :drand
+
+
+  def initialize(brand)
+    @brand = brand
+  
+  end 
+  
+  def condition=(show_conditon)
+    @condition = show_conditon
+  end 
+
+  def condition
+    @condition
+  end 
+
+  def cobble=(condition_status)
+    @cobble = condition_status
+  end 
+
+  def cobble
+    @cobble 
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end 
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Shoe
+#     attr_accessor :brand, :color, :size, :material, :condition
+  
+#     def initialize(brand)
+#       @brand = brand 
+#     end
+  
+#     def brand
+#       @brand
+#     end
+  
+#     def cobble
+
+#         puts "Your shoe is as good as new!"
+#         @condition = "new"
+#     end
+    
+#   end 
+
